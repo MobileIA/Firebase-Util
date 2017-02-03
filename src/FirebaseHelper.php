1e3b7720545f1206708b446c0df210efe72543c3
Matias Camiletti <matias.camiletti@gmail.com>
2017-02-03 11:25:22 -0300

    Comienzo del helper.

---
+++ src/FirebaseHelper.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace MobileIA\Firebase;
+
+use Zend\Http\Request;
+use Zend\Http\Client;
+use Zend\Stdlib\Parameters;
+use Zend\Json\Json;
+
+/**
+ * Description of Helper
+ *
+ * @author matiascamiletti
+ */
+class FirebaseHelper 
+{
+    /**
+     * Almacena la URL base de la API.
+     */
+    const BASE_URL = 'https://fcm.googleapis.com/fcm/send';
+    
+    /**
+     * 
+     * @param array $ids
+     * @param array $data
+     * @return array
+     */
+    public function sendToRegistrationsIds($ids, $data)
+    {
+        // Creamos la peticion con los parametros necesarios
+        $request = $this->generateRequest(array(
+            'registration_ids' => $ids,
+            'data' => $data
+        ));
+        // Ejecutamos la petición
+        $response = $this->dispatchRequest($request);
+
+        return $response;
+    }
+    /**
+     * 
+     * @param string $topicName
+     * @param array $params
+     * @return array
+     */
+    public function sendTopic($topicName, $params)
+    {
+        // Creamos la peticion con los parametros necesarios
+        $request = $this->generateRequest(array(
+            'to' => '/topics/'.$topicName,
+            'data' => $params
+        ));
+        // Ejecutamos la petición
+        $response = $this->dispatchRequest($request);
+        
+        return $response;
+    }
+    /**
+     * Realiza la peticion y devuelve los parametros
+     * @param Request $request
+     * @return array
+     */
+    protected function dispatchRequest($request)
+    {
+        $client = new Client();
+        $response = $client->dispatch($request);
+        return Json::decode($response->getBody());
+    }
+    /**
+     * Genera un request con el path y los parametros
+     * @param string $path
+     * @param array $params
+     * @return Request
+     */
+    protected function generateRequest($params)
+    {
+        $request = new Request();
+        $request->getHeaders()->addHeaders(array(
+            'Content-Type' => 'application/json',
+            'Authorization' => 'key=AIzaSyCi8L3TV-BBKzAs57BWfH2R8A0v-QlZB3Q'
+        ));
+        $request->setUri(self::BASE_URL);
+        $request->setMethod(Request::METHOD_POST);
+        $request->setContent(Json::encode($params));
+        $request->setPost(new Parameters($params));
+        
+        return $request;
+    }
+}
